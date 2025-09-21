100d8f614f16883951eb4404cc7590c4ed911c6d
Mukdaniella <mugabodaniella22@gmail.com>
2025-09-22 00:38:15 +0200

    after i went on loading saving and rendering tasks

---
+++ assets/index.js
@@ -0,0 +1,32 @@
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+const darkToggle = document.getElementById('dark-mode-toggle');
+const searchInput = document.getElementById('search-input');
+const taskPriority = document.getElementById('task-priority');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTasks(filter = '') {
+    taskList.innerHTML = '';
+
+    tasks
+        .filter(task => task.text.toLowerCase().includes(filter.toLowerCase()))
+        .forEach((task, index) => {
+            const li = document.createElement('li');
+            li.className = `flex justify-between items-center p-2 border rounded hover:shadow cursor-pointer 
+                            ${task.completed ? 'completed' : ''} ${task.priority}`;
+
+            li.innerHTML = `
+                <span onclick="toggleComplete(${index})" ondblclick="editTask(${index})">${task.text}</span>
+                <button onclick="deleteTask(${index})" class="text-red-500 font-bold ml-2">X</button>
+            `;
+
+            taskList.appendChild(li);
+        });
+}
